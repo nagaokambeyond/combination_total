8c55d401166336eb157b6394bacf730cd0fd681e
nagaokambeyond <40172017+nagaokambeyond@users.noreply.github.com>
2022-12-15 22:45:04 +0900

    リファクタリング
    lint

--- python/python.py
+++ python/python.py
@@ -1,15 +1,16 @@
+""" 合計がXになる整数の組み合わせを出力する """
 source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-"""
-https://qiita.com/Cesaroshun/items/6302542e4e32602d775d
-
-Args:
-    list:リスト
-    target:この値になる
-Returns:
-    targetになる組み合わせのリスト
-"""
 def get_integral_value_combination(list, target):
+    """
+    https://qiita.com/Cesaroshun/items/6302542e4e32602d775d
+
+    Args:
+        list:リスト
+        target:この値になる
+    Returns:
+        targetになる組み合わせのリスト
+    """
     def a(idx, l, r, t):
         if t == sum(l): r.append(l)
         elif t < sum(l): return
@@ -18,18 +19,29 @@ def get_integral_value_combination(list, target):
         return r
     return a(0, [], [], target)
 
-def get_integral_value_combination_refacter(list, target_value):
-    list_length = len(list)
+def get_integral_value_combination_refacter(list_source: list, target_value: int) -> list:
+    """
+    https://qiita.com/Cesaroshun/items/6302542e4e32602d775d
+
+    Args:
+        list_source:リスト
+        target_value:この値になる
+    Returns:
+        target_valueになる組み合わせのリスト
+    """
+    list_length = len(list_source)
     result = []
-    def recursion(start_index, combination_list):
+    def recursion(start_index: int, combination_list: list) -> None:
         summary = sum(combination_list)
-        if target_value == summary: 
+        if target_value == summary:
             result.append(combination_list)
-        elif target_value < summary: 
+        elif target_value < summary:
             return  # 超えているためなにも返さない
         else:
             for index in range(start_index, list_length):
-                recursion((index + 1), combination_list + [list[index]])
+                next_start_index = index + 1
+                next_combination_list = combination_list + [list_source[index]]    # 指定要素の内容を追加したリスト
+                recursion(next_start_index, next_combination_list)
         return
     recursion(0, [])
     return result
